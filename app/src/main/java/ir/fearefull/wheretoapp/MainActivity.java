178f65633e287c54b067f46ac8e3b2ec32a8b581
Aref <arefhosseini@yahoo.com>
2019-04-12 19:14:42 +0430

    First Commit

---
+++ app/src/main/java/ir/fearefull/wheretoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ir.fearefull.wheretoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
