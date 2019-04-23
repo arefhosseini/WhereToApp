f3c5c07ec53530657385aa037e46f52317eb5686
Aref <arefhosseini@yahoo.com>
2019-04-24 04:01:28 +0430

    Complete Fragment Home in PlaceActivity such as CollapsingToolbarLayout, GoogleMap Place Information, Place Images & Rating & Reviews Fix Some Bugs

--- app/src/main/java/ir/fearefull/wheretoapp/PlaceActivity.java
+++ app/src/main/java/ir/fearefull/wheretoapp/PlaceActivity.java
@@ -2,33 +2,30 @@ package ir.fearefull.wheretoapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import ir.fearefull.wheretoapp.Adapter.MainPager;
+import ir.fearefull.wheretoapp.Enum.PlaceTypeEnum;
 import ir.fearefull.wheretoapp.Fragment.PlaceHomeFragment;
 import ir.fearefull.wheretoapp.Fragment.PlaceMenuFragment;
 import ir.fearefull.wheretoapp.Fragment.PlaceReviewFragment;
 import ir.fearefull.wheretoapp.Model.Place;
-import ir.fearefull.wheretoapp.Model.PlaceSummary;
 import ir.fearefull.wheretoapp.Network.GetDataService;
 import ir.fearefull.wheretoapp.Network.RetrofitClientInstance;
+import me.zhanghai.android.materialratingbar.MaterialRatingBar;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -38,8 +35,11 @@ public class PlaceActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private MainPager viewPager;
-    private TextView placeNameTextView, tabLayoutTextTextView;
+    private TextView placeNameTextView, tabLayoutTextTextView, placeOverallScoreTextView,
+            placePriceDegreeTextView, placePriceDegreeUnselectedTextView, placeTypeTextView;
     private ImageView placeImageImageView;
+    private LinearLayout placeTypesLayout;
+    private MaterialRatingBar placeOverallScoreRatingBar;
 
     private int lastSelectedTab = 0;
 
@@ -64,6 +64,11 @@ public class PlaceActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabs);
         placeNameTextView = findViewById(R.id.placeNameTextView);
         placeImageImageView = findViewById(R.id.placeImageImageView);
+        placeOverallScoreTextView = findViewById(R.id.placeOverallScoreTextView);
+        placeOverallScoreRatingBar = findViewById(R.id.placeOverallScoreRatingBar);
+        placePriceDegreeTextView = findViewById(R.id.placePriceDegreeTextView);
+        placePriceDegreeUnselectedTextView = findViewById(R.id.placePriceDegreeUnselectedTextView);
+        placeTypesLayout = findViewById(R.id.placeTypesLayout);
 
         Intent myIntent = getIntent(); // gets the previously created intent
         long id = myIntent.getLongExtra("id", 0);
@@ -90,6 +95,15 @@ public class PlaceActivity extends AppCompatActivity {
         this.place = place;
         placeNameTextView.setText(place.getName());
         Picasso.get().load(Constants.BASE_URL + place.getPlaceImage()).into(placeImageImageView);
+        placeOverallScoreRatingBar.setRating(place.getOverallScore());
+        placeOverallScoreTextView.setText(String.valueOf("(" + String.valueOf(place.getAllScoresCount()) + ")"));
+        placePriceDegreeTextView.setText(repeat(place.getPriceDegree(), "$"));
+        placePriceDegreeUnselectedTextView.setText(repeat(4 - place.getPriceDegree(), "$"));
+        for (PlaceTypeEnum placeTypeEnum: place.getPlaceTypes()) {
+            placeTypeTextView = (TextView) getLayoutInflater().inflate(R.layout.card_place_type, placeTypesLayout, false);
+            placeTypeTextView.setText(placeTypeEnum.getText());
+            placeTypesLayout.addView(placeTypeTextView);
+        }
 
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
@@ -173,4 +187,10 @@ public class PlaceActivity extends AppCompatActivity {
         Objects.requireNonNull(tabLayout.getTabAt(2)).select();
 
     }
+
+    public static String repeat(int count, String with) {
+        return new String(new char[count]).replace("\0", with);
+    }
 }
+
+
